fab52b935e6aca0bc1015b1361dd2130851094ae
Tom Fewster <tom@wannabegeek.com>
2016-03-14 15:42:09 +0000

    Install google test correctly

--- travis.sh
+++ travis.sh
@@ -5,7 +5,7 @@ env | sort
 cd $TRAVIS_BUILD_DIR
 mkdir external || true
 wget -qO- https://sourceforge.net/projects/boost/files/boost/1.60.0/boost_1_60_0.tar.gz/download | tar xz -C external/
-export BOOST_ROOT=external/boost_1_60_0/
+export BOOST_ROOT=$TRAVIS_BUILD_DIR/external/boost_1_60_0/
 
 cd $TRAVIS_BUILD_DIR
 wget -qO googletest.zip https://github.com/google/googletest/archive/master.zip
@@ -14,7 +14,9 @@ mkdir -p build/external/googletest
 cd build/external/googletest
 cmake -DCMAKE_BUILD_TYPE=RELEASE $TRAVIS_BUILD_DIR/external/googletest-master
 make
-export GTEST_ROOT=external/googletest-master/
+export GTEST_ROOT=$TRAVIS_BUILD_DIR/external/googletest-master/googletest
+mkdir -p $GTEST_ROOT/lib
+cp lib*.a $GTEST_ROOT/lib
 
 cd $TRAVIS_BUILD_DIR
 mkdir build || true
