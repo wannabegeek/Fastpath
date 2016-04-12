7d3feb507bd48b3d87983b72c2baad3ebe30a791
Tom Fewster <tom@wannabegeek.com>
2016-04-12 08:58:56 +0100

    Added different data fields for different sizes

---
+++ src/messages/LargeDataField.h
@@ -0,0 +1,16 @@
+//
+// Created by Tom Fewster on 12/04/2016.
+//
+
+#ifndef TFDCF_LARGEDATAFIELD_H
+#define TFDCF_LARGEDATAFIELD_H
+
+#include "DataField.h"
+
+namespace DCF {
+    class SmallDataField : public DataField {
+
+    };
+}
+
+#endif //TFDCF_LARGEDATAFIELD_H
---
+++ src/messages/SmallDataField.h
@@ -0,0 +1,16 @@
+//
+// Created by Tom Fewster on 12/04/2016.
+//
+
+#ifndef TFDCF_SMALLDATAFIELD_H
+#define TFDCF_SMALLDATAFIELD_H
+
+#include "DataField.h"
+
+namespace DCF {
+    class SmallDataField : public DataField {
+
+    };
+}
+
+#endif //TFDCF_SMALLDATAFIELD_H
