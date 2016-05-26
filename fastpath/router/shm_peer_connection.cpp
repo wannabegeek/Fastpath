7b58f8b4ec2a3eb0ae06a058a8b4c4d2dc945621
Tom Fewster <tom@wannabegeek.com>
2016-05-26 07:49:01 +0100

    Added copyright

--- fastpath/router/shm_peer_connection.cpp
+++ fastpath/router/shm_peer_connection.cpp
@@ -1,6 +1,27 @@
-//
-// Created by Tom Fewster on 23/05/2016.
-//
+/***************************************************************************
+                          __FILE__
+                          -------------------
+    copyright            : Copyright (c) 2004-2016 Tom Fewster
+    email                : tom@wannabegeek.com
+    date                 : 26/03/2016
+
+ ***************************************************************************/
+
+/***************************************************************************
+ * This library is free software; you can redistribute it and/or           *
+ * modify it under the terms of the GNU Lesser General Public              *
+ * License as published by the Free Software Foundation; either            *
+ * version 2.1 of the License, or (at your option) any later version.      *
+ *                                                                         *
+ * This library is distributed in the hope that it will be useful,         *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       *
+ * Lesser General Public License for more details.                         *
+ *                                                                         *
+ * You should have received a copy of the GNU Lesser General Public        *
+ * License along with this library; if not, write to the Free Software     *
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA *
+ ***************************************************************************/
 
 #include "shm_peer_connection.h"
 
@@ -39,7 +60,6 @@ namespace fp {
               m_process_id(std::move(other.m_process_id)),
               m_socketEvent(std::move(other.m_socketEvent)),
               m_notifierEvent(std::move(other.m_notifierEvent)) {
-
     }
 
     shm_peer_connection::~shm_peer_connection() noexcept {
